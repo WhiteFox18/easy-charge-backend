202a6f26af3df66722af8eed1ecaa50579564b22
WhiteFox18 <sharafiddinovfazliddin@gmail.com>
2022-06-04 10:47:02 +0500

    fixes for triggers

--- constants/migrations/2_extensions.sql
+++ constants/migrations/2_extensions.sql
@@ -19,15 +19,17 @@ $$;
 CREATE OR REPLACE FUNCTION update_admin_full_name() RETURNS trigger AS
 $$
 BEGIN
-    UPDATE admins
-    SET full_name = concat(name, ' ', surname, ' ', patronymic)
-    WHERE id = NEW.id;
+    IF (OLD.id = NEW.id OR OLD.id IS NULL) THEN
+        UPDATE admins
+        SET full_name = concat(username, ' ', name, ' ', surname, ' ', patronymic)
+        WHERE id = NEW.id;
+    END IF;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER after_admin_inserted_updated
-    AFTER INSERT OR UPDATE
+    AFTER INSERT OR UPDATE OF username, name, surname, patronymic
     ON admins
     FOR EACH ROW
 EXECUTE PROCEDURE update_admin_full_name();
@@ -36,15 +38,17 @@ EXECUTE PROCEDURE update_admin_full_name();
 CREATE OR REPLACE FUNCTION update_company_user_full_name() RETURNS trigger AS
 $$
 BEGIN
-    UPDATE company_user
-    SET full_name = concat(name, ' ', surname, ' ', patronymic, ' ', phone_number, ' ', passport)
-    WHERE id = NEW.id;
+    IF (OLD.id = NEW.id OR OLD.id IS NULL) THEN
+        UPDATE company_user
+        SET full_name = concat(name, ' ', surname, ' ', patronymic, ' ', phone_number, ' ', passport)
+        WHERE id = NEW.id;
+    END IF;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER after_company_user_inserted_updated
-    AFTER INSERT OR UPDATE
+    AFTER INSERT OR UPDATE OF name, surname, patronymic, phone_number, passport
     ON company_user
     FOR EACH ROW
 EXECUTE PROCEDURE update_company_user_full_name();
@@ -53,15 +57,17 @@ EXECUTE PROCEDURE update_company_user_full_name();
 CREATE OR REPLACE FUNCTION update_company_branch_full_name() RETURNS trigger AS
 $$
 BEGIN
-    UPDATE company_branch
-    SET full_name = concat(name_uz, ' ', name_ru, ' ', name_en)
-    WHERE id = NEW.id;
+    IF (OLD.id = NEW.id OR OLD.id IS NULL) THEN
+        UPDATE company_branch
+        SET full_name = concat(name_uz, ' ', name_ru, ' ', name_en)
+        WHERE id = NEW.id;
+    END IF;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER after_company_branch_inserted_updated
-    AFTER INSERT OR UPDATE
+    AFTER INSERT OR UPDATE OF name_uz, name_ru, name_en
     ON company_branch
     FOR EACH ROW
 EXECUTE PROCEDURE update_company_branch_full_name();
@@ -70,15 +76,17 @@ EXECUTE PROCEDURE update_company_branch_full_name();
 CREATE OR REPLACE FUNCTION update_country_full_name() RETURNS trigger AS
 $$
 BEGIN
-    UPDATE countries
-    SET full_name = concat(name_uz, ' ', name_ru, ' ', name_en)
-    WHERE id = NEW.id;
+    IF (OLD.id = NEW.id OR OLD.id IS NULL) THEN
+        UPDATE countries
+        SET full_name = concat(name_uz, ' ', name_ru, ' ', name_en)
+        WHERE id = NEW.id;
+    END IF;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER after_country_inserted_updated
-    AFTER INSERT OR UPDATE
+    AFTER INSERT OR UPDATE OF name_uz, name_ru, name_en
     ON countries
     FOR EACH ROW
 EXECUTE PROCEDURE update_country_full_name();
@@ -87,15 +95,17 @@ EXECUTE PROCEDURE update_country_full_name();
 CREATE OR REPLACE FUNCTION update_state_full_name() RETURNS trigger AS
 $$
 BEGIN
-    UPDATE states
-    SET full_name = concat(name_uz, ' ', name_ru, ' ', name_en)
-    WHERE id = NEW.id;
+    IF (OLD.id = NEW.id OR OLD.id IS NULL) THEN
+        UPDATE states
+        SET full_name = concat(name_uz, ' ', name_ru, ' ', name_en)
+        WHERE id = NEW.id;
+    END IF;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER after_states_inserted_updated
-    AFTER INSERT OR UPDATE
+    AFTER INSERT OR UPDATE OF name_uz, name_ru, name_en
     ON states
     FOR EACH ROW
 EXECUTE PROCEDURE update_state_full_name();
@@ -104,15 +114,17 @@ EXECUTE PROCEDURE update_state_full_name();
 CREATE OR REPLACE FUNCTION update_city_full_name() RETURNS trigger AS
 $$
 BEGIN
-    UPDATE cities
-    SET full_name = concat(name_uz, ' ', name_ru, ' ', name_en)
-    WHERE id = NEW.id;
+    IF (OLD.id = NEW.id OR OLD.id IS NULL) THEN
+        UPDATE cities
+        SET full_name = concat(name_uz, ' ', name_ru, ' ', name_en)
+        WHERE id = NEW.id;
+    END IF;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER after_city_inserted_updated
-    AFTER INSERT OR UPDATE
+    AFTER INSERT OR UPDATE OF name_uz, name_ru, name_en
     ON cities
     FOR EACH ROW
 EXECUTE PROCEDURE update_city_full_name();
@@ -121,15 +133,17 @@ EXECUTE PROCEDURE update_city_full_name();
 CREATE OR REPLACE FUNCTION update_district_full_name() RETURNS trigger AS
 $$
 BEGIN
-    UPDATE districts
-    SET full_name = concat(name_uz, ' ', name_ru, ' ', name_en)
-    WHERE id = NEW.id;
+    IF (OLD.id = NEW.id OR OLD.id IS NULL) THEN
+        UPDATE districts
+        SET full_name = concat(name_uz, ' ', name_ru, ' ', name_en)
+        WHERE id = NEW.id;
+    END IF;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER after_district_inserted_updated
-    AFTER INSERT OR UPDATE
+    AFTER INSERT OR UPDATE OF name_uz, name_ru, name_en
     ON districts
     FOR EACH ROW
 EXECUTE PROCEDURE update_district_full_name();
@@ -138,15 +152,17 @@ EXECUTE PROCEDURE update_district_full_name();
 CREATE OR REPLACE FUNCTION update_fuel_type_full_name() RETURNS trigger AS
 $$
 BEGIN
-    UPDATE fuel_types
-    SET full_name = concat(name_uz, ' ', name_ru, ' ', name_en)
-    WHERE id = NEW.id;
+    IF (OLD.id = NEW.id OR OLD.id IS NULL) THEN
+        UPDATE fuel_types
+        SET full_name = concat(name_uz, ' ', name_ru, ' ', name_en)
+        WHERE id = NEW.id;
+    END IF;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER after_fuel_type_inserted_updated
-    AFTER INSERT OR UPDATE
+    AFTER INSERT OR UPDATE OF name_uz, name_ru, name_en
     ON fuel_types
     FOR EACH ROW
 EXECUTE PROCEDURE update_fuel_type_full_name();
